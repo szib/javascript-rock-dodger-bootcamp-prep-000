57f8e19c2a86851b228239e1fc6fee0321f0280e
Ivan Szebenszki <szib77@gmail.com>
2019-03-02 20:56:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-    if(checkCollision()) {
+    if(checkCollision(rock)) {
       endGame()
     }
     /**
