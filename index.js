c638439daad2055a1a39ed0513b699d25e4c3172
Ivan Szebenszki <szib77@gmail.com>
2019-03-02 20:14:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function checkCollision(rock) {
       return rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge
     }
   
-  return true
+  return false
   // if (top > 360) {
   //   if (collisionCheck1 || collisionCheck2 || collisionCheck3) {
   //     return true
