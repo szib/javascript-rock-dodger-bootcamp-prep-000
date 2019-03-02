0b5fca76940f4a67f5d978af56c69758c0e66812
Ivan Szebenszki <szib77@gmail.com>
2019-03-02 21:12:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function checkCollision(rock) {
        */
       
       if ((rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) || 
-          (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge) || 
+          (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) || 
           (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)) {
           return true
       } 
