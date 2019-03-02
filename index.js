6ef0d91b406ed10813e633d0cf78a1e6fd1d1734
Ivan Szebenszki <szib77@gmail.com>
2019-03-02 20:27:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -152,6 +152,11 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   if(e.which === LEFT_ARROW || e.which === RIGHT_ARROW) {
+     e.preventDefault();
+     e.stopPropagation();
+   }
+   
    if (e.which === LEFT_ARROW) {
      moveDodgerLeft()
    } else if (e.which === RIGHT_ARROW ) {
