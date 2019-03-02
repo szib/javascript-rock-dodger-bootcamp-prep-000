2e305454b79b04b76a1b5d5e29e6f922f8984de4
Ivan Szebenszki <szib77@gmail.com>
2019-03-02 19:54:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,26 +30,26 @@ function checkCollision(rock) {
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
   
-      const dodgerLeftEdge = positionToInteger(DODGER.style.left);
+  const dodgerLeftEdge = positionToInteger(DODGER.style.left);
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge += DODGER_WIDTH;
+  const dodgerRightEdge += DODGER_WIDTH;
 
-    const rockLeftEdge = positionToInteger(rock.style.left)
+  const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge += 20;
+  const rockRightEdge += 20;
   
-      /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
-               */
+    /**
+     * Think about it -- what's happening here?
+     * There's been a collision if one of three things is true:
+     * 1. The rock's left edge is < the DODGER's left edge,
+     *    and the rock's right edge is > the DODGER's left edge;
+     * 2. The rock's left edge is > the DODGER's left edge,
+     *    and the rock's right edge is < the DODGER's right edge;
+     * 3. The rock's left edge is < the DODGER's right edge,
+     *    and the rock's right edge is > the DODGER's right edge
+     */
     function collisionCheck1() {
       return rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge
     }
