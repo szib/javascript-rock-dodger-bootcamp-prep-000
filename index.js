bc8a36ad5594cd7141bfaeafd621c6be0feeee75
Ivan Szebenszki <szib77@gmail.com>
2019-03-02 20:24:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -138,6 +138,9 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval)
   
+  for(let i = 0; i<ROCKS.length; i++) {
+    ROCKS[i].remove()
+  }
 }
 
 function moveDodger(e) {
